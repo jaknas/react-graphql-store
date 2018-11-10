f38846d8fb074b56c3c106dd68a06fd901c99b07
kleyu <jackob.naskreski@gmail.com>
2018-11-11 00:22:21 +0100

    implement login signup and reset password flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -7,7 +8,18 @@ const db = require('./db');
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO use express middleware to populate current user
+
+// decode the JWT to get the userID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put userId on the req for further requests to access
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
