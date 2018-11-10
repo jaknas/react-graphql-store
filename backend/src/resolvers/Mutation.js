9c3d01a7d8fde81fa1863019e226ffa77ab0de76
kleyu <jackob.naskreski@gmail.com>
2018-11-10 01:28:04 +0100

    add signup backend logic

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -34,6 +37,31 @@ const Mutations = {
 
     // delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    //hash user's password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+    // create JWT token for user
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return user to the browser
+    return user;
   }
 };
 
