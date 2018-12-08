ed9b36ba4db61e519c32961277804a83ec38fce4
kleyu <jackob.naskreski@gmail.com>
2018-12-08 01:05:18 +0100

    add some tests

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,39 @@
+import ItemComponent from '../components/Item';
+import { shallow, mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A cool item',
+  price: 5000,
+  description: 'big cool think',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg'
+};
+
+describe('<Item/>', () => {
+  it('renders and matches the snapshot', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+  it('renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+  it('renders the priceag and title', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const PriceTag = wrapper.find('PriceTag');
+    expect(PriceTag.children().text()).toBe('$50');
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('DeleteItem')).toHaveLength(1);
+    expect(buttonList.find('AddToCart')).toHaveLength(1);
+  });
+});
